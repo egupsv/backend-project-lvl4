aa567891e5c2e21cf5d27ab3aabd0d24277137f7
Sergey Egupov <egupsv@mail.ru>
2020-01-10 23:41:09 +0400

    first commit: only greeting

---
+++ src/bin/hello.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import Koa from 'koa';
+import Rollbar from 'rollbar';
+
+const rollbar = new Rollbar({
+  accessToken: 'POST_SERVER_ITEM_ACCESS_TOKEN',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+const app = new Koa();
+app.use(async (ctx, next) => {
+  try {
+    await next();
+    ctx.body = 'hello';
+  } catch (err) {
+    rollbar.error(err, ctx.request);
+  }
+});
+
+app.listen(3000);
